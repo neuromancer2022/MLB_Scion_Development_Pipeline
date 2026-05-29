55b2dd3a171597b0128c4c217a860294a1794dde
Jon Tepper <jtepper@perceptronix.net>
2026-05-29 18:25:49 +0100

    active dev

--- src/MLB_Scion_dGEN.py
+++ src/MLB_Scion_dGEN.py
@@ -4495,7 +4495,8 @@ class scionDGEN:
         #d. populate lookback fields
         self._populateLookbackFields(h_or_v, fileHandle)
         #e. populate lookahead fields (WE CAN ONLY USE GAMES WITHIN THE SAME SEASON)
-        self._populateLookAheadFields(h_or_v, fileHandle)
+        if self._lookahead_active:
+            self._populateLookAheadFields(h_or_v, fileHandle)
         #f. calculate window-based statistics (ensure flags are correctly set before each call _calcFeatureStats)
         self._writeWindowEntryBanner(fileHandle)
         fileHandle.write("\nCalculating window-based averages...")
